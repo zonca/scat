019fbf7b323ddbc18d0664804106ffcc5da47016
Andrea Zonca <code@andreazonca.com>
2022-05-25 00:43:13 +0000

    first working example

---
+++ scat/__init__.py
@@ -0,0 +1,3 @@
+from .root import read_root_file
+
+from .daskutils import dask_memory_usage_GB, dask_histogram
---
+++ scat/root.py
@@ -0,0 +1,9 @@
+import uproot3
+import pandas as pd
+
+def read_root_file(filename):
+    with uproot3.open(filename) as f:
+        df = pd.DataFrame({k.decode("utf-8"):v.copy() for k,v in f["data"].arrays().items()})
+    # dask doesn't support multiindex
+    # df.set_index(["seriesNumber", "eventNumber", "detNumber"])
+    return df
