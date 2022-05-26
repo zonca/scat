3979b10a8fd4377f5d9919932800ddf3b00b960f
Andrea Zonca <code@andreazonca.com>
2022-05-26 00:58:38 +0000

    completed with pipeline

--- scat/__init__.py
+++ scat/__init__.py
@@ -2,4 +2,4 @@ from .root import read_root_file
 
 from .daskutils import dask_memory_usage_GB, dask_histogram
 
-from .config import load_filter
+from .config import load_filter, Pipeline
--- scat/config.py
+++ scat/config.py
@@ -1,8 +1,22 @@
 import toml
 from .filters import *
-
+from dataclasses import dataclass
+from typing import List
 
 def load_filter(toml_file):
     config = toml.load(toml_file)
     filter_class = globals()[config.pop("class")]
     return filter_class(**config)
+
+@dataclass
+class Pipeline:
+
+    steps : List[str]
+
+    def __post_init__(self):
+        self.filters = list(map(load_filter, self.steps))
+
+    def apply(self, df):
+        for filt in self.filters:
+            df = df.map_partitions(filt)
+        return df
