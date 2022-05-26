ea6153550f179a9fde2957bb9fd10d4e9227f26a
Andrea Zonca <code@andreazonca.com>
2022-05-26 01:39:42 +0000

    black

--- tdat/config.py
+++ tdat/config.py
@@ -3,15 +3,17 @@ from .filters import *
 from dataclasses import dataclass
 from typing import List
 
+
 def load_filter(toml_file):
     config = toml.load(toml_file)
     filter_class = globals()[config.pop("class")]
     return filter_class(**config)
 
+
 @dataclass
 class Pipeline:
 
-    steps : List[str]
+    steps: List[str]
 
     def __post_init__(self):
         self.filters = list(map(load_filter, self.steps))
